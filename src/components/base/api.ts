b948abff6b3c947a8015924b32333865e448078c
Sergey Mikolenko <mmmaybach@yandex.ru>
2024-03-23 15:08:33 +0500

    Исправил название файлов

--- src/components/base/api.ts
+++ src/components/base/api.ts
@@ -1,6 +1,6 @@
 export type ApiListResponse<Type> = {
-    total: number;  // Общее количество элементов
-    items: Type[];  // Массив элементов указанного типа
+	total: number; // Общее количество элементов
+	items: Type[]; // Массив элементов указанного типа
 };
 
 // Определяем типы HTTP-методов, поддерживаемых API
@@ -8,62 +8,69 @@ export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
 // Базовый класс для работы с API
 export class Api {
-    readonly baseUrl: string;       // URL базового адреса API
-    protected options: RequestInit; // Опции запроса
+	readonly baseUrl: string; // URL базового адреса API
+	protected options: RequestInit; // Опции запроса
 
-    /**
-     * Создает новый экземпляр класса Api
-     * @constructor
-     * @param {string} baseUrl - Базовый URL API
-     * @param {RequestInit} options - Опции запроса
-     */
-    constructor(baseUrl: string, options: RequestInit = {}) {
-        this.baseUrl = baseUrl;
-        this.options = {
-            headers: {
-                'Content-Type': 'application/json', // Устанавливаем заголовок Content-Type для запросов в формате JSON
-                ...(options.headers as object ?? {}) // Объединяем дополнительные заголовки из параметров конструктора
-            }
-        };
-    }
+	/**
+	 * Создает новый экземпляр класса Api
+	 * @constructor
+	 * @param {string} baseUrl - Базовый URL API
+	 * @param {RequestInit} options - Опции запроса
+	 */
+	constructor(baseUrl: string, options: RequestInit = {}) {
+		this.baseUrl = baseUrl;
+		this.options = {
+			headers: {
+				'Content-Type': 'application/json', // Устанавливаем заголовок Content-Type для запросов в формате JSON
+				...((options.headers as object) ?? {}), // Объединяем дополнительные заголовки из параметров конструктора
+			},
+		};
+	}
 
-    /**
-     * Обрабатывает ответ от сервера
-     * @param {Response} response - Ответ от сервера
-     * @returns {Promise<object>} - Обработанный JSON-объект или сообщение об ошибке
-     */
-    protected handleResponse(response: Response): Promise<object> {
-        if (response.ok) 
-            return response.json(); // Если ответ успешный (статус код 2xx), возвращаем JSON
-        else 
-            return response.json() // Иначе, обрабатываем JSON ответа, который может содержать ошибку, или возвращаем текст ошибки по умолчанию
-                .then(data => Promise.reject(data.error ?? response.statusText));
-    }
+	/**
+	 * Обрабатывает ответ от сервера
+	 * @param {Response} response - Ответ от сервера
+	 * @returns {Promise<object>} - Обработанный JSON-объект или сообщение об ошибке
+	 */
+	protected handleResponse(response: Response): Promise<object> {
+		if (response.ok)
+			return response.json(); // Если ответ успешный (статус код 2xx), возвращаем JSON
+		else
+			return response
+				.json() // Иначе, обрабатываем JSON ответа, который может содержать ошибку, или возвращаем текст ошибки по умолчанию
+				.then((data) => Promise.reject(data.error ?? response.statusText));
+	}
 
-    /**
-     * Выполняет GET-запрос к API
-     * @param {string} uri - Путь к конечной точке API
-     * @returns {Promise<object>} - Обработанный ответ от сервера
-     */
-    get(uri: string): Promise<object> {
-        return fetch(this.baseUrl + uri, { // Выполняем GET-запрос к указанной конечной точке API
-            ...this.options, // Передаем опции запроса (заголовки и др.)
-            method: 'GET'   // Устанавливаем метод запроса GET
-        }).then(this.handleResponse); // Обрабатываем полученный ответ от сервера
-    }
+	/**
+	 * Выполняет GET-запрос к API
+	 * @param {string} uri - Путь к конечной точке API
+	 * @returns {Promise<object>} - Обработанный ответ от сервера
+	 */
+	get(uri: string): Promise<object> {
+		return fetch(this.baseUrl + uri, {
+			// Выполняем GET-запрос к указанной конечной точке API
+			...this.options, // Передаем опции запроса (заголовки и др.)
+			method: 'GET', // Устанавливаем метод запроса GET
+		}).then(this.handleResponse); // Обрабатываем полученный ответ от сервера
+	}
 
-    /**
-     * Выполняет POST-запрос к API
-     * @param {string} uri - Путь к конечной точке API
-     * @param {object} data - Данные, передаваемые в теле запроса (в формате JSON)
-     * @param {ApiPostMethods} method - HTTP-метод запроса (по умолчанию 'POST')
-     * @returns {Promise<object>} - Обработанный ответ от сервера
-     */
-    post(uri: string, data: object, method: ApiPostMethods = 'POST'): Promise<object> {
-        return fetch(this.baseUrl + uri, { // Выполняем POST-запрос к указанной конечной точке API
-            ...this.options, // Передаем опции запроса (заголовки и др.)
-            method,          // Устанавливаем метод запроса (может быть POST, PUT или DELETE)
-            body: JSON.stringify(data) // Передаем данные запроса в виде JSON-строки
-        }).then(this.handleResponse); // Обрабатываем полученный ответ от сервера
-    }
+	/**
+	 * Выполняет POST-запрос к API
+	 * @param {string} uri - Путь к конечной точке API
+	 * @param {object} data - Данные, передаваемые в теле запроса (в формате JSON)
+	 * @param {ApiPostMethods} method - HTTP-метод запроса (по умолчанию 'POST')
+	 * @returns {Promise<object>} - Обработанный ответ от сервера
+	 */
+	post(
+		uri: string,
+		data: object,
+		method: ApiPostMethods = 'POST'
+	): Promise<object> {
+		return fetch(this.baseUrl + uri, {
+			// Выполняем POST-запрос к указанной конечной точке API
+			...this.options, // Передаем опции запроса (заголовки и др.)
+			method, // Устанавливаем метод запроса (может быть POST, PUT или DELETE)
+			body: JSON.stringify(data), // Передаем данные запроса в виде JSON-строки
+		}).then(this.handleResponse); // Обрабатываем полученный ответ от сервера
+	}
 }
--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -1,71 +1,81 @@
 // Создание алиасов для типов данных
 type EventName = string | RegExp; // Имя события может быть строкой или регулярным выражением
 type Subscriber = Function; // Подписчик - функция, обрабатывающая событие
-type EmitterEvent = { // Тип данных для события, содержащего имя и данные
-    eventName: string,
-    data: unknown
+type EmitterEvent = {
+	// Тип данных для события, содержащего имя и данные
+	eventName: string;
+	data: unknown;
 };
 
 // Интерфейс, определяющий методы для работы с событиями
 export interface IEvents {
-    // Метод для подписки на событие
-    on<T extends object>(event: EventName, callback: (data: T) => void): void;
-    // Метод для инициирования события
-    emit<T extends object>(event: string, data?: T): void;
-    // Метод для создания триггера события
-    trigger<T extends object>(event: string, context?: Partial<T>): (data: T) => void;
+	// Метод для подписки на событие
+	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+	// Метод для инициирования события
+	emit<T extends object>(event: string, data?: T): void;
+	// Метод для создания триггера события
+	trigger<T extends object>(
+		event: string,
+		context?: Partial<T>
+	): (data: T) => void;
 }
 
 // Реализация класса для управления событиями
 export class EventEmitter implements IEvents {
-    _events: Map<EventName, Set<Subscriber>>; // Хранилище событий и подписчиков
+	_events: Map<EventName, Set<Subscriber>>; // Хранилище событий и подписчиков
 
-    constructor() {
-        this._events = new Map<EventName, Set<Subscriber>>(); // Инициализация хранилища
-    }
+	constructor() {
+		this._events = new Map<EventName, Set<Subscriber>>(); // Инициализация хранилища
+	}
 
-    // Метод для подписки на событие
-    on<T extends object>(eventName: EventName, callback: (event: T) => void) {
-        if (!this._events.has(eventName)) { // Проверяем, есть ли уже подписчики на это событие
-            this._events.set(eventName, new Set<Subscriber>()); // Если нет, создаем новый набор
-        }
-        this._events.get(eventName)?.add(callback); // Добавляем подписчика
-    }
+	// Метод для подписки на событие
+	on<T extends object>(eventName: EventName, callback: (event: T) => void) {
+		if (!this._events.has(eventName)) {
+			// Проверяем, есть ли уже подписчики на это событие
+			this._events.set(eventName, new Set<Subscriber>()); // Если нет, создаем новый набор
+		}
+		this._events.get(eventName)?.add(callback); // Добавляем подписчика
+	}
 
-    // Метод для отписки от события
-    off(eventName: EventName, callback: Subscriber) {
-        if (this._events.has(eventName)) { // Проверяем, есть ли подписчики на это событие
-            this._events.get(eventName)!.delete(callback); // Удаляем подписчика из набора
-            if (this._events.get(eventName)?.size === 0) { // Если больше нет подписчиков, удаляем событие из хранилища
-                this._events.delete(eventName);
-            }
-        }
-    }
+	// Метод для отписки от события
+	off(eventName: EventName, callback: Subscriber) {
+		if (this._events.has(eventName)) {
+			// Проверяем, есть ли подписчики на это событие
+			this._events.get(eventName)!.delete(callback); // Удаляем подписчика из набора
+			if (this._events.get(eventName)?.size === 0) {
+				// Если больше нет подписчиков, удаляем событие из хранилища
+				this._events.delete(eventName);
+			}
+		}
+	}
 
-    // Метод для инициирования события
-    emit<T extends object>(eventName: string, data?: T) {
-        this._events.forEach((subscribers, name) => {
-            // Проверяем, подходит ли событие под переданный шаблон или является ли событие именно тем, которое нужно вызвать
-            if (name instanceof RegExp && name.test(eventName) || name === eventName) {
-                subscribers.forEach(callback => callback(data)); // Вызываем всех подписчиков этого события
-            }
-        });
-    }
+	// Метод для инициирования события
+	emit<T extends object>(eventName: string, data?: T) {
+		this._events.forEach((subscribers, name) => {
+			// Проверяем, подходит ли событие под переданный шаблон или является ли событие именно тем, которое нужно вызвать
+			if (
+				(name instanceof RegExp && name.test(eventName)) ||
+				name === eventName
+			) {
+				subscribers.forEach((callback) => callback(data)); // Вызываем всех подписчиков этого события
+			}
+		});
+	}
 
-    // Метод для подписки на все события
-    onAll(callback: (event: EmitterEvent) => void) {
-        this.on("*", callback); // Добавляем обработчик ко всем событиям
-    }
+	// Метод для подписки на все события
+	onAll(callback: (event: EmitterEvent) => void) {
+		this.on('*', callback); // Добавляем обработчик ко всем событиям
+	}
 
-    // Метод для отписки от всех событий
-    offAll() {
-        this._events = new Map<string, Set<Subscriber>>(); // Очищаем хранилище событий и подписчиков
-    }
+	// Метод для отписки от всех событий
+	offAll() {
+		this._events = new Map<string, Set<Subscriber>>(); // Очищаем хранилище событий и подписчиков
+	}
 
-    // Метод для создания триггера события
-    trigger<T extends object>(eventName: string, context?: Partial<T>) {
-        return (event: object = {}) => {
-            this.emit(eventName, { ...(event || {}), ...(context || {}) }); // Инициируем событие с переданными данными
-        };
-    }
+	// Метод для создания триггера события
+	trigger<T extends object>(eventName: string, context?: Partial<T>) {
+		return (event: object = {}) => {
+			this.emit(eventName, { ...(event || {}), ...(context || {}) }); // Инициируем событие с переданными данными
+		};
+	}
 }
